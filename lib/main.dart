56fe80707b325f30b800ff6a38da17c3f3325bc9
Sadaf Zakria <102993150+sadafzakria@users.noreply.github.com>
2023-08-28 17:17:11 -0400

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,52 @@
 import 'package:flutter/material.dart';
 
-//starting point calling the method runApp
 void main() {
   runApp(MyApp());
 }
+/*
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
-//runApp is calling class MyApp
-//myApp is building the materialApp which is the root of all flutter widget
-//MyApp instantiates another class MyStatelessWidget which creates the Scaffold
+  static const String _title = "Flutter Demo";
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: _title,
+      home: MYStatelessWidget(),
+    );
+  }
+}
+
+class MYStatelessWidget extends StatelessWidget {
+  const MYStatelessWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter Demo'),
+        actions: [
+          IconButton(
+              onPressed: () {
+                ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(content: Text('Pressed bell button')));
+              },
+              icon: Icon(Icons.add_alert))
+        ],
+      ),
+      body: Center(
+        child: Text(
+          'Hello World',
+          style: TextStyle(fontSize: 24),
+        ),
+      ),
+    );
+  }
+}
+*/
+
 class MyApp extends StatelessWidget {
   MyApp({super.key});
 
@@ -22,50 +61,113 @@ class MyApp extends StatelessWidget {
   }
 }
 
-/*
-//MyStatelessWidget designs the widget Scaffold, that user designs
-//in the main screen(body) of the device
-class MyStatelessWidget extends StatelessWidget {
-  MyStatelessWidget({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title:Text("Revisting Flutter"),),
-      body: Center(
-        child: Text(
-          'Welcome Back',
-          style: TextStyle(fontSize: 30),
-        ),
-      ),
-    );
-  }
-}
-
- */
 class MyStatelessWidget extends StatelessWidget {
   const MyStatelessWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
+    int counter = 1;
     return Scaffold(
         appBar: AppBar(
           title: Text("Revisting Flutter"),
         ),
-        body: Center(
-            child: Column(
+        body: ListView(
           children: [
             SizedBox(
               height: 100,
             ),
             Container(
               height: 150,
-              width: 200,
-              color: Colors.green,
+              width: 105,
+              color: Colors.blueAccent,
               alignment: Alignment.center,
-              child: Text('Hey Container'),
+              child: Text(
+                'Hello World Hello flutter',
+                style: TextStyle(fontSize: 20, color: Colors.white),
+              ),
             ),
+            Text('The Sky is Blue',
+                style: TextStyle(fontSize: 20, color: Colors.lightBlue)),
+            ElevatedButton(
+                onPressed: () {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(
+                          content: Text('Pressed CLICK ME button'),
+                      ),
+                  );
+
+                },
+                child: Text('Click me')),
+
+            ElevatedButton(
+
+                onPressed: () {
+
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(
+
+                      content: Text('${counter}') ,
+
+                    ),
+                  );
+                  counter= counter+1;
+                },
+                child: Text('ADD')),
+            ElevatedButton(
+
+                onPressed: () {
+
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(
+
+                      content: Text('${counter}') ,
+
+                    ),
+                  );
+                  counter= counter-1;
+                },
+                child: Text('SUBTRACT')),
+            Container(
+              height: 150,
+              width: 150,
+              color: Colors.blue,
+              alignment: Alignment.center,
+              child: Text(
+                'Hello World3 Hello flutter3',
+                style: TextStyle(fontSize: 20),
+              ),
+            ),
+            Container(
+              height: 250,
+              width: 250,
+              color: Colors.lightBlueAccent,
+              alignment: Alignment.center,
+              child: Image(
+                image: NetworkImage(
+                    "https://media.istockphoto.com/id/1328689113/photo/summer-blue-sky-and-white-cloud-white-background-beautiful-clear-cloudy-in-sunlight-calm.jpg?s=612x612&w=0&k=20&c=37qEuwdxyQSx9kuS-_Gz0WiKFX6jMXZN9aRY47mN2vI="),
+              ),
+            ),
+            Container(
+              height: 150,
+              width: 150,
+              color: Colors.blue,
+              alignment: Alignment.center,
+              child: Text(
+                'Hello World3 Hello flutter3',
+                style: TextStyle(fontSize: 20),
+              ),
+            ),
+            Container(
+              height: 250,
+              width: 250,
+              color: Colors.lightBlueAccent,
+              alignment: Alignment.center,
+              child: Image(
+                image: NetworkImage(
+                    "https://img.freepik.com/free-photo/cloud-sky-twilight-times_74190-4017.jpg?w=1380&t=st=1693255713~exp=1693256313~hmac=51bbaa5b32ff77fd324f3ca2a6e63f9350488bc9d3c808aa861e5236d512396f"),
+              ),
+            )
           ],
-        )));
+        ));
   }
 }
--- lib/test.dart
+++ lib/test.dart
@@ -1,80 +1,158 @@
+//import 'dart:js_util';
+import 'dart:io';
+
+import 'dart:ffi';
+
 void main() {
-  //1
-  print("example of adding two numbers using the function");
-
-  int sum(int a, int b) {
-    int result;
-    result = a + b;
-    return result;
-  }
-
-  var c = sum(30, 20);
-  print("the sum of the two numbers is: ${c}");
-
-  //2
-  void number(int n) {
-    if (n % 2 == 0) {
-      print("even");
-    } else {
-      print("odd");
-    }
-  }
-
-  number(20);
-
-  //3
-  var list = ['james', 'mathew', 'tom'];
-  print("Example of anonymous function");
-  list.forEach((item) {
-    print('${list.indexOf(item)}: $item');
-  });
-
-  //4
-  //anonymous function using function expression
-  var multiply = (int a, int b) {
-    return a * b;
-  };
-  //anonymous function using lambda
-  var multiplying = (int a, int b) => a * b;
-  var result = multiplying(5, 3);
-  print(result);
-
-  //5
-  //recursion
-  var num = 5;
-  var fact = factiorial(num);
-  print("Factorial = ${fact}");
-
-  //6
-  //Class and Object (Student class below)
-  var std =  new Student();
+  var std = new Student();
   std.stdName = "Peter";
   std.stdAge = 24;
   std.stdRoll_nu = 9001;
-  //show info.
+  Student.stdBranch = "Computer Science";
+
+//show info. (class function)
   std.showStdInfo();
 
-}
-//end of main()
+  //var std2 = new Student("Jonas",26);
 
-//recursive function to find factorial
-int factiorial(int num) {
-  if (num <= 1) {
-    return 1;
-  } else {
-    return num * factiorial(num - 1);
+  Child c = new Child();
+
+  Bike b = new Bike();
+  b.display();
+
+  Parrot p = new Parrot();
+  p.speak();
+  p.fly();
+
+  //implicit superclass constructor
+  Subclass s = new Subclass();
+  s.display();
+
+  double tempF = 86;
+  double result = (tempF - 32) / 1.8;
+
+  print("${tempF.toInt()}F = ${result.toInt()}C");
+
+  var list = [1, 3, 5, 7, 9];
+  var sum = 0;
+  for (var value in list) {
+    sum += value;
+  }
+  print(sum);
+
+  print('Enter X');
+  var x = double.parse(stdin.readLineSync()!);
+  print('Enter Y');
+  var y = double.parse(stdin.readLineSync()!);
+
+  var sum2 = x+y;
+  print('sum ${sum2}');
+  var average = sum2/2;
+  print('average ${average}');
+} //end of main
+
+class Triangle {
+  void print(String st) {
+    String st = "*";
+    for (int i = 0; i < 5; i++) {
+      print(st);
+      st+="*";
+
+    }
   }
 }
 
-//Student class
-class Student{
-  var stdName;
-  var stdAge;
-  var stdRoll_nu;
+class Student {
+  static var stdBranch; //static variable belongs to the class not the object
+  String _stdName = "null";
+  int _stdAge = 0;
+  int _stdRoll_nu = 0;
 
-  showStdInfo(){
+//getters + setters
+
+  //Age
+  int get stdAge => _stdAge;
+
+  set stdAge(int value) {
+    if (stdAge <= 0) {
+      print("Age should be greater than 5");
+    } else {
+      _stdAge = value;
+    }
+  }
+
+  //Roll Number
+  int get stdRoll_nu => _stdRoll_nu;
+
+  set stdRoll_nu(int value) {
+    _stdRoll_nu = value;
+  }
+
+  //Name
+  String get stdName => _stdName;
+
+  set stdName(String value) {
+    _stdName = value;
+  }
+
+  showStdInfo() {
     print("Name ${stdName}");
     print("Age ${stdAge}");
     print("Roll Number ${stdRoll_nu}");
+    print("Branch ${stdBranch}");
+  }
+}
+
+class Parent {
+  Parent() {
+    print("super-class");
+  }
+}
+
+//inheritance "super" keyword is used with the constructor
+class Child extends Parent {
+  Child() : super() {
+    print("sub-class");
+  }
+}
+
+class Car {
+  var speed = 180;
+}
+
+class Bike extends Car {
+  var speed = 110;
+
+  void display() {
+    print("Speed: ${super.speed}");
+  }
+}
+
+class Bird {
+  void fly() {
+    print("Bird can fly");
+  }
+}
+
+class Parrot extends Bird {
+  void speak() {
+    print("Parrot can speak");
+  }
+}
+
+class Superclass {
+  Superclass(String msg) {
+    print("Superclass Constructor");
+    print(msg);
+  }
+}
+
+class Subclass extends Superclass {
+  Subclass() : super("calling superclass constructor explicitly") {
+    print("subclass constructor");
+  }
+
+  display() {
+    print("welcome to dart");
   }
 }
